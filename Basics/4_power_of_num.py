33a35815230983f981ce8a6d86304e3dc6a35d6b
Megha Sawant <42641952+meghasawant@users.noreply.github.com>
2018-08-31 21:38:45 +0530

    Power of number

--- Basics/4_power_of_num.py
+++ Basics/4_power_of_num.py
@@ -1,8 +1,8 @@
 ######### Assignment No.4- WAP to accept a 2 numbers from user & display the power of that number.
 
-n1 = input("Enter number1 -")
+n1 = input("Enter number 1 -")
 
-n2 = input("Enter number2 -")
+n2 = input("Enter number 2 -")
 
 print n1,"^",n2,"is",n1**n2
 
