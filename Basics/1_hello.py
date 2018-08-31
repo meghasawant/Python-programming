86d0fefaeca71d1893d80b66d954badd14b9afa5
Megha Sawant <42641952+meghasawant@users.noreply.github.com>
2018-08-31 21:30:05 +0530

    hello world

---
+++ Basics/1_hello.py
@@ -0,0 +1,9 @@
+######### Assignment No.1- WAP to accept name from user & greet them  with hello.
+
+name = raw_input('Enter your name - ')	#accept string input
+
+#print("Hello",name)			#print name
+
+print 
+print "Hello",name,"Welcome!!!"
+
